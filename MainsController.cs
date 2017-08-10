28c262900cea3230344d505ed54e5ae8a39f3406
toriboone <vkishman88@gmail.com>
2017-08-10 10:43:16 -0400

    Controllers for handling database interactions

---
+++ MainsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Moodwave5.Models;
+
+namespace Moodwave5.Controllers
+{
+    public class MainsController : Controller
+    {
+        private Moodwave1Entities db = new Moodwave1Entities();
+
+        // GET: Mains
+        public ActionResult Index()
+        {
+            var mains = db.Mains.Include(m => m.User).Include(m => m.Wave);
+            return View(mains.ToList());
+        }
+
+        // GET: Mains/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        // GET: Mains/Create
+        public ActionResult Create()
+        {
+            ViewBag.userID = new SelectList(db.Users, "userID", "Email");
+            ViewBag.waveID = new SelectList(db.Waves, "waveID", "Mood");
+            return View();
+        }
+
+        // POST: Mains/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userID,waveID,songID")] Main main)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Mains.Add(main);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userID = new SelectList(db.Users, "userID", "Email", main.userID);
+            ViewBag.waveID = new SelectList(db.Waves, "waveID", "Mood", main.waveID);
+            return View(main);
+        }
+
+        // GET: Mains/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userID = new SelectList(db.Users, "userID", "Email", main.userID);
+            ViewBag.waveID = new SelectList(db.Waves, "waveID", "Mood", main.waveID);
+            return View(main);
+        }
+
+        // POST: Mains/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "userID,waveID,songID")] Main main)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(main).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userID = new SelectList(db.Users, "userID", "Email", main.userID);
+            ViewBag.waveID = new SelectList(db.Waves, "waveID", "Mood", main.waveID);
+            return View(main);
+        }
+
+        // GET: Mains/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        // POST: Mains/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Main main = db.Mains.Find(id);
+            db.Mains.Remove(main);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UsersController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Moodwave5.Models;
+
+namespace Moodwave5.Controllers
+{
+    public class UsersController : Controller
+    {
+        private Moodwave1Entities db = new Moodwave1Entities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Main);
+            return View(users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.userID = new SelectList(db.Mains, "userID", "songID");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Email,Password,userID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userID = new SelectList(db.Mains, "userID", "songID", user.userID);
+            return View(user);
+        }
+        
+        public ActionResult LogIn([Bind(Include = "Email,Password,userID")] User u)
+        {
+            return View();
+        }
+
+        public ActionResult checkLogIn([Bind(Include = "Email,Password,userID")] User u)
+        {
+            bool matchFound = false;
+            List<User> users = db.Users.ToList();
+            foreach (User i in users)
+            {
+                if (u.Email == i.Email && u.Password == i.Password)
+                {
+                    matchFound = true;
+                }
+            }
+            if (matchFound == true)
+            {
+                return RedirectToAction("Index", "Mains");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userID = new SelectList(db.Mains, "userID", "songID", user.userID);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Email,Password,userID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userID = new SelectList(db.Mains, "userID", "songID", user.userID);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
